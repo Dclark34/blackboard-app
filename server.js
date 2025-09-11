247661cd385046ba251e4a39bd5f7f2c58f60fea
DClark <duncan.clark528@gmail.com>
2025-09-11 14:31:35 -0400

    Deploy to Heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const path = require('path');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || "3000";
 
 
 
